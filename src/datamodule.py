17afafaa45a820dd9938f95600034f0c870795a5
Martin Georgiu <martin.georgiu@gmail.com>
2023-02-08 16:18:54 +0100

    add shuffle

--- src/datamodule.py
+++ src/datamodule.py
@@ -112,7 +112,7 @@ class HAM10000DataModule(pl.LightningDataModule):
 
     def train_dataloader(self):
         # TODO: Add more train data when doing these augmentations
-        return DataLoader(self.ham_train, batch_size=self.batch_size, num_workers=0)
+        return DataLoader(self.ham_train, batch_size=self.batch_size, shuffle=True, num_workers=0)
 
     def val_dataloader(self):
         return DataLoader(self.ham_val, batch_size=self.batch_size, num_workers=0)
